a2488d87b078336c263c31e77b5c19be7cfdc648
Cris <245176630+IzarTheGr8@users.noreply.github.com>
2025-12-15 16:11:42 +0100

    first commit

---
+++ caesar_cipher.rb
@@ -0,0 +1,23 @@
+ALPHABET = ("a".."z").to_a
+
+def caesar_cipher(input, shift = 0)
+  input.each_char.map do |letter|
+    letter_index = ALPHABET.index(letter.downcase)
+
+    if letter_index
+      new_letter_index = letter_index + shift
+      new_letter_index -= ALPHABET.length unless new_letter_index < ALPHABET.length
+      letter == letter.upcase ? letter = ALPHABET[new_letter_index].upcase : letter = ALPHABET[new_letter_index]
+    else
+      letter
+    end
+  end.join
+end
+
+puts 'Input:'
+input = gets.chomp
+
+puts 'Shift factor:'
+shift = gets.chomp.to_i
+
+puts caesar_cipher(input, shift)
